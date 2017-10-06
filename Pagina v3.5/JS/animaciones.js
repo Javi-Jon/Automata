f07ac3c05064308f069ad11cf86a336a33574fa7
javiermdg998 <javiermdg998@gmail.com>
2017-10-06 14:05:49 +0200

    Add files via upload

---
+++ Pagina v3.5/JS/animaciones.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Javi y Jon on 5/10/17.
+ */
+//document.getElementById('p1').style.animationDuration = 'red'
+
+//creacion de animacion para el siguiente movimiento del tranvia
+    //mirar si meter mejor en un metodo(!)
+var animacion = CSSAnimations
+
+anim.setKeyframe(100% {transform:"translateX("+movimiento+" px)"});
